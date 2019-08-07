db724511544027ac3122b61106083aa9844a59fc
Zantezu <danilo.patrucco@gmail.com>
2019-08-06 23:16:51 -0400

    Task : added answer form and added delete option to the answer

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -122,7 +122,7 @@ class AnswerController extends Controller
         $answer = Answer::find($answer);
 
         $answer->delete();
-        return redirect()->route('questions.show',['question_id' => $question])->with('message', 'Delete');
+        return redirect()->route('questions.show',['question_id' => $question])->with('message', 'Deleted');
 
     }
 }
